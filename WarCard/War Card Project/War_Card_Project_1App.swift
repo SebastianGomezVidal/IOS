625af6b2b4936a74c296fcf23e49cee2f8f23e21
Sebastian Gomez Vidal <juanquindio@gmail.com>
2022-07-10 19:20:48 -0500

    Loading Project

---
+++ WarCard/War Card Project/War_Card_Project_1App.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_Project_1App.swift
+//  War Card Project 1
+//
+//  Created by Juan Gomez on 9/05/22.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_Project_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
